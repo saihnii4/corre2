8f47a261d7b657f35fdbe4481c13d5e8f268105a
saihnii4 <81787757+saihnii4@users.noreply.github.com>
2023-01-20 14:32:36 +0800

    ir sketch

---
+++ ir/ir.ino
@@ -0,0 +1,29 @@
+/*
+ * IRremote: IRrecvDemo - demonstrates receiving IR codes with IRrecv
+ * An IR detector/demodulator must be connected to the input RECV_PIN.
+ * Version 0.1 July, 2009
+ * Copyright 2009 Ken Shirriff
+ * http://arcfn.com
+ */
+ 
+#include <IRremote.hpp>
+ 
+int RECV_PIN = 5;
+ 
+decode_results results;
+ 
+void setup()
+{
+  Serial.begin(9600);
+  IrReceiver.begin(5, true); //
+}
+void loop() {
+  if (IrReceiver.decode()) {
+      Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX); // Print "old" raw data
+      IrReciever.decodedI
+      /* USE NEW 3.x FUNCTIONS */
+      IrReceiver.printIRResultShort(&Serial); // Print complete received data in one line
+      IrReceiver.printIRSendUsage(&Serial);   // Print the statement required to send this data
+      IrReceiver.resume(); // Enable receiving of the next value
+  }
+}
